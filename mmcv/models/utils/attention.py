476c52999ec6377c82c6f543059b4dd6dd593a67
lintsao <ethentsao1998@gmail.com>
2025-11-14 06:57:33 +0000

    Fix import.

--- mmcv/models/utils/attention.py
+++ mmcv/models/utils/attention.py
@@ -17,9 +17,9 @@ from torch.nn.functional import linear
 from einops import rearrange
 from mmcv.utils import auto_fp16
 # for H20 flash-attn-2.7.0.post2 is ok
-from flash_attn.flash_attn_interface import flash_attn_varlen_kvpacked_func
+# from flash_attn.flash_attn_interface import flash_attn_varlen_kvpacked_func
 # for A800/A100
-# from flash_attn.flash_attn_interface import flash_attn_unpadded_kvpacked_func
+from flash_attn.flash_attn_interface import flash_attn_unpadded_kvpacked_func as flash_attn_varlen_kvpacked_func
 from flash_attn.bert_padding import unpad_input, pad_input, index_first_axis
 
 
